2033a403cc3c575f393e773fb25e578e26404296
Vladimir Semakov <jsvohome@gmail&com>
2022-02-15 07:35:53 +0300

    Optimisation to LiveData

--- app/src/main/java/ru/rucobe/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/rucobe/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,24 +1,36 @@
 package ru.rucobe.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import ru.rucobe.shoppinglist.domain.ShopItem
 import ru.rucobe.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository{
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     //установили переменную, храним в переменной для данного примера 0
     private val shopList = mutableListOf<ShopItem>()
 
     private var autoIncrementId = 0
 
+    init {
+        for(i in 0 until 10){
+            val item = ShopItem("Name $i",i,true)
+            addShopItem(item)
+        }
+    }
     override fun addShopItem(shopItem: ShopItem) {
         if(shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -31,7 +43,11 @@ object ShopListRepositoryImpl: ShopListRepository{
         return shopList.find {it.id == shopItemId} ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 }
